6428bed2f8d353aba775b801214ef801fc48bd8f
PedramHashemi <s.pedram.h@gmail.com>
2023-07-19 18:52:35 +0200

    adding tests and changing the model

--- starter/ml/model.py
+++ starter/ml/model.py
@@ -24,7 +24,6 @@ def train_model(X_train, y_train):
     clf.fit(X_train, y_train)
     return clf
 
-
 def compute_model_metrics(y, preds):
     """
     Validates the trained machine learning model using precision, recall, and F1.
@@ -41,11 +40,11 @@ def compute_model_metrics(y, preds):
     recall : float
     fbeta : float
     """
+
     fbeta = fbeta_score(y, preds, beta=1, zero_division=1)
     precision = precision_score(y, preds, zero_division=1)
     recall = recall_score(y, preds, zero_division=1)
     return precision, recall, fbeta
-
 
 def inference(model: RandomForestClassifier, X):
     """ Run model inferences and return the predictions.
@@ -61,4 +60,5 @@ def inference(model: RandomForestClassifier, X):
     preds : np.array
         Predictions from the model.
     """
+
     return model.predict(X)
---
+++ starter/tests/test_model.py
@@ -0,0 +1,84 @@
+"""Test for model functions."""
+
+from starter.ml.model import inference, train_model, compute_model_metrics
+from starter.ml.data import process_data
+import pytest
+import pandas as pd
+import joblib
+from sklearn.ensemble import RandomForestClassifier
+
+cat_features = [
+    "workclass",
+    "education",
+    "marital-status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native-country",
+]
+
+@pytest.fixture(name='data')
+def data():
+    """
+    Census data as fixture.
+    """
+    return pd.read_csv("../starter/data/census.csv")
+
+@pytest.fixture(name='lb')
+def lb():
+    """
+    label binerizer for the preprocessing the data.
+    """
+    return joblib.load("starter/ml/model/lb.pkl")
+
+@pytest.fixture(name='encoder')
+def encoder():
+    """
+    encoder for the preprocessing the data.
+    """
+    return joblib.load("starter/ml/model/encoder.pkl")
+
+@pytest.fixture(name='sample')
+def sample():
+    """
+    Sample data for inference.
+    """
+    return data.iloc[0]
+
+def test_train_model():
+    """
+    Test if the model is a random forest.
+    """
+
+    model = joblib.load("starter/ml/model/model.pkl")
+    assert isinstance(model, RandomForestClassifier)
+
+def test_inference(sample):
+    """Generate the inference on a sample."""
+    model = joblib.load("starter/ml/model/model.pkl")
+    x_sample, _, _, _ = process_data(
+        sample,
+        cat_features,
+        label='salary',
+        encoder=encoder,
+        lb=lb
+    )
+    assert round(inference(model, x_sample)).isin([0, 1])
+
+def test_compute_model_metrics():
+    """Test the metrics."""
+    model = joblib.load("starter/ml/model/model.pkl")
+    x_sample, label, _, _ = process_data(
+        sample,
+        cat_features,
+        label='salary',
+        encoder=encoder,
+        lb=lb
+    )
+    pred = round(inference(model, x_sample))
+    metrics = compute_model_metrics(label, pred)
+    assert len(metrics) == 3
+    assert metrics[0] <= 1
+    assert metrics[1] <= 1
+    assert metrics[2] <= 1
