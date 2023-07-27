56ff5ced418d433f6c6b2e8e6d4108530568ebd1
PedramHashemi <s.pedram.h@gmail.com>
2023-07-27 21:44:07 +0200

    update main and model

--- starter/main.py
+++ starter/main.py
@@ -115,4 +115,4 @@ def infer(profile: Profile):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=1000, reload=True)
